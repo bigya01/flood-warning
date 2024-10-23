f6f92364adf2c7293621d8ed410618d307cb80f9
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-23 23:59:38 +0545

    removing mongodb

--- backend/routes/rainfallRoutes.js
+++ backend/routes/rainfallRoutes.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const axios = require('axios');
-const Rainfall = require('../models/Rainfall');
 const router = express.Router();
+const https = require('https');
 
-// Fetch rainfall data from API and save it to MongoDB
+const agent = new https.Agent({  
+    rejectUnauthorized: false
+});
+// Fetch rainfall data from API
 router.get('/fetch', async (req, res) => {
     try {
-        const response = await axios.get('https://www.dhm.gov.np/frontend_dhm/hydrology/getRainfallFilter');
+        const response = await axios.get('https://www.dhm.gov.np/frontend_dhm/hydrology/getRainfallFilter', { httpsAgent: agent });        console.log('API Response', response.data);
+        
         const currentRainfall = response.data.totalRainfall; // Adjust based on your data structure
-
-        // Save to database
-        const rainfallRecord = new Rainfall({ totalRainfall: currentRainfall });
-        await rainfallRecord.save();
-
+        console.log('Current Rainfall: ', currentRainfall);
+        
+        // Return the rainfall data directly in the response
         res.json({ success: true, totalRainfall: currentRainfall });
     } catch (error) {
         console.error(error);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const rainfallRoutes = require('./routes/rainfallRoutes');
 
 
 
 // Load environment variables from .env file
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,9 +20,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error(err));
 
 // Start the server
 app.listen(PORT, () => {
