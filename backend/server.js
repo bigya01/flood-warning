6ec9338e20e5599d207293e592488996d95146cf
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-24 16:29:51 +0545

    rainfall_data added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use('/api/rainfall', rainfallRoutes);
 
