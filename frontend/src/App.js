d3d5c6dedcbc626fad2949dfc700622ea6b8b7fe
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-26 14:20:37 +0545

    ui fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,14 @@
-// import React, { useEffect } from 'react';
-// import MapComponent from './MapComponent';
-// import RainfallDisplay from './RainfallDisplay'; // Import your RainfallDisplay
 
-// const App = () => {
-//     const redMarkers = [
-//         { lat: 27.700769, lng: 85.300140 }, // Kathmandu
-//         // { lat: 27.6713, lng: 85.324 },
-//         {lat : 27.7509337, lng : 85.3320221},
-        
-//         {lat : 29.97166667, lng : 81.81611111},
-
-//         {lat : 28.2096, lng : 83.9856}
-//     ];
-
-//     const greenMarkers = [
-//         { lat: 27.1780, lng: 87.0524},
-//         { lat: 27.6880, lng: 85.3250 }
-//     ];
-
-//     useEffect(() => {
-//         const checkProximity = (userLocation) => {
-//             const RADIUS = 5000; // 5 km radius
-//             redMarkers.forEach(marker => {
-//                 const distance = getDistance(userLocation, marker);
-//                 if (distance <= RADIUS) {
-//                     sendNotification("You are near a red marker!");
-//                 }
-//             });
-
-//         };
-
-//         const sendNotification = (message) => {
-//             if (Notification.permission === 'granted') {
-//                 new Notification("Alert!", { body: message });
-//             } else if (Notification.permission !== 'denied') {
-//                 Notification.requestPermission().then(permission => {
-//                     if (permission === 'granted') {
-//                         new Notification("Alert!", { body: message });
-//                     }
-//                 });
-//             }
-//         };
-
-//         const getDistance = (loc1, loc2) => {
-//             const toRad = (value) => (Math.PI / 180) * value;
-//             const R = 6371e3; // Earth radius in meters
-//             const φ1 = toRad(loc1.lat);
-//             const φ2 = toRad(loc2.lat);
-//             const Δφ = toRad(loc2.lat - loc1.lat);
-//             const Δλ = toRad(loc2.lng - loc1.lng);
-
-//             const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-//                       Math.cos(φ1) * Math.cos(φ2) *
-//                       Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-//             const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//             return R * c; // Distance in meters
-//         };
-
-//         // Get user location
-//         navigator.geolocation.getCurrentPosition((position) => {
-//             const userLocation = {
-//                 lat: position.coords.latitude,
-//                 lng: position.coords.longitude,
-//             };
-//             checkProximity(userLocation);
-//         }, (error) => {
-//             console.error("Geolocation error: ", error);
-//         });
-//     }, []);
-
-//     return (
-//         <div>
-//             <MapComponent 
-//                 redMarkers={redMarkers} 
-//                 greenMarkers={greenMarkers} 
-//                 initialLatitude={28.3949} 
-//                 initialLongitude={84.1240} 
-//                 zoomLevel={6} 
-//             />
-//             <RainfallDisplay /> 
-//         </div>
-//     );
-// };
-
-// export default App;
 
 import React, { useEffect } from "react";
 import MapComponent from "./MapComponent";
+import Visualization from "./Frontend";
 import RainfallDisplay from "./RainfallDisplay"; // Import your RainfallDisplay
-// import FloodMonitoringDashboard from "./Frontend";
+
 import { FaHome, FaInfo, FaPhone, FaEnvelope } from "react-icons/fa"; // Import icons
 import './App.css';
+// import './frontend.css';
+
 const App = () => {
   const redMarkers = [
     { lat: 27.700769, lng: 85.30014 }, // Kathmandu
@@ -190,6 +108,7 @@ const App = () => {
             zoomLevel={6}
           />
           {/* <FloodMonitoringDashboard /> */}
+          <Visualization />
           <RainfallDisplay />
         </div>
       </div>
---
+++ frontend/src/Frontend.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import scatter from './assets/scatter.png';
+import percep from './assets/percep.png';
+import precipitation from './assets/precipittion.png';
+// const imagePaths = [
+
+//   './assets/precipittion.png',
+
+// ];
+const Visualization = () => {
+  return (
+    <div className='all'>
+      <h2>Precipitation Analysis</h2>
+
+      <div className="visualization-container">
+        <div className='percep'>
+          <img src={percep} alt="Precipitation Scatter Plot" />
+        </div>
+        <div className='preciptiation'>
+          <img src={precipitation} alt="Precipitation Scatter Plot" />
+        </div>
+        {/* {imagePaths.map((path, index) => (
+        <img key={index} src={require(`${path}`)} alt={`Visualization ${index + 1}`} />
+      ))} */}
+
+      </div>
+      <div>
+        <img src={scatter} alt="Precipitation Scatter Plot" />
+      </div>
+
+    </div>
+
+  );
+};
+
+export default Visualization;
