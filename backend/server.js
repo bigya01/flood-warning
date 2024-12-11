d17b8e6078bfef1952569d20f01400909361cf1a
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-12-11 20:40:21 +0545

    fixes in backend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 console.log("Setting up /api/rainfall route");
 app.use('/api/rainfall', rainfallRoutes);
